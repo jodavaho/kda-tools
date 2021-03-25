c7d79aed2c0b001db101d395a0ad4caa237206e1
Josh Vander Hook <hook@jpl.nasa.gov>
2021-03-24 22:34:26 -0700

    Init

---
+++ src/bin/kda-stretch.rs
@@ -0,0 +1,29 @@
+//use std::collections::HashMap;
+use std::io::BufRead;
+use std::io;
+//use std::fs::File;
+//use std::path::Path;
+//use std::io::BufRead;
+use std::string::String;
+
+fn main() {
+    let mut line_count = 0;
+    for input_line in io::stdin().lock().lines()
+    {
+        let line:String = match input_line
+        {
+            Ok(line)=>line,
+            Err(_)=>continue,
+        };
+        let mut tok_iter = line.split_whitespace();
+        let indexer = match tok_iter.next()
+        {
+            Some(tok)=>tok,
+            None=>continue,
+        };
+        for tok in tok_iter{
+            println!("{} {} {}",line_count,indexer,tok);
+        }
+        line_count+=1;
+    }
+}
