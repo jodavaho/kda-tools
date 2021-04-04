ae6181c1683c03fb06232e9bdc0620a1522e5578
Josh Vander Hook <hook@jpl.nasa.gov>
2021-04-04 15:13:46 -0700

    '*' and '_' working

--- src/bin/kda-compare.rs
+++ src/bin/kda-compare.rs
@@ -34,7 +34,7 @@ fn main() -> Result<(),String> {
     let local_sin = stdin();
     let line_itr = local_sin.lock().lines();
 
-    let ( size, data, names ) = kvc::load_table_from_kvc_stream_default(line_itr);
+    let ( size,data, names ) = kvc::load_table_from_kvc_stream_default(line_itr);
     let ( num_matches, num_vars ) = size;
     eprintln!("Processed. Read: {} rows and {} variables", num_matches,num_vars);
     if num_matches==0{
@@ -64,15 +64,27 @@ fn main() -> Result<(),String> {
     let mut ins:Vec<String> = Vec::new();
     if inout.len()==2{
        ins= inout[1].split("vs").map(|x| x.trim().to_string()).collect() ;
-    } 
+    } else {
+        assert_eq!(ins.len(),0);
+    }
+
+    //verify all input variables
     let mut in_idxs:HashSet<usize> = HashSet::new();
     for idx in 0..ins.len(){
-        let variable_name = &ins[idx];
-        assert!( idx_lookup.contains_key(variable_name),std::format!("Variable '{}' not found in input data.",ins[idx]));
-        in_idxs.insert( *idx_lookup.get(variable_name).unwrap() );
+        let variable_name:String = ins[idx].clone();
+        eprintln!("Received input: {}",variable_name);
+        match &variable_name[..]{
+            //don't bother verifying reserved keywords at this stage
+            "_"|"*"=>(),
+            _=>{
+                assert!( idx_lookup.contains_key(&variable_name),std::format!("Variable '{}' not found in input data.",ins[idx]));
+                in_idxs.insert( *idx_lookup.get(&variable_name).unwrap() );
+            },
+        }
     }
 
 
+    //verify all output variables
     let outs:Vec<String> = inout[0].split_whitespace().map(|x| x.to_string()).collect();
     let mut out_idxs:HashSet<usize> = HashSet::new();
     for idx in 0..outs.len(){
@@ -80,31 +92,75 @@ fn main() -> Result<(),String> {
         out_idxs.insert( *idx_lookup.get(&outs[idx]).unwrap() );
     }
 
-    //for each input, I
-    //for each outout, x, calculate pdf(x|I)
-
     //create a handy data looker-upper
-    let data_lookup:HashMap<(usize,usize),f32> = data.into_iter().collect();
+    let data_lookup:HashMap<(usize,usize),String> = data.clone().into_iter().collect();
     if ins.len()==0{
         ins.push( "_".to_string() );
     }
 
-
+    //create the relevant pairings to iterate over for experiments
+    let mut split_list:Vec<Vec<usize>> = Vec::new();
+    let mut split_names:Vec<String> = Vec::new();
     for out_experiment in ins{
-        let relevant_matches = match &out_experiment[..]{
+        match &out_experiment[..]{
+            "*"=> {
+                eprintln!("Fetching: '*' as 'all individual inputs'");
+                //there's one special case, "*" which means all-to-all compare,
+                //in which case we can append all inputs as single experiments
+                for variable_name in names.clone(){
+                    let data_idx = *idx_lookup.get(&variable_name[..]).unwrap();
+                    //for which matches did that interesting event occur?
+                    let to_insert = data.clone().into_iter()
+                        //filter first by idx matching the one in question
+                        .filter(| ((_,idx),_) |  *idx==data_idx )
+                        //and return only those rows
+                        .map(| ((time,_),_) | time ).collect::<Vec<_>>();
+                    split_list.push(to_insert);
+                    split_names.push(variable_name.clone());
+                }
+            },
             "_"=> {
-                (0..num_matches).collect::<Vec<_>>()
+                eprintln!("Fetching: '_' as 'baseline'");
+                //compare vs all data "baseline"
+                split_list.push( (0..num_matches).collect::<Vec<_>>() );
+                split_names.push("Baseline".to_string());
             },
             _=>{
+                //user specifically requested some data by name.
                 //what column of the data corresponds to the  interesting one?
-                eprintln!("Fetching: {}",out_experiment);
+                assert!(idx_lookup.contains_key(&out_experiment[..]),"Could not find {} in input data!",out_experiment);
+                eprintln!("Fetching {} by name",out_experiment);
                 let data_idx = *idx_lookup.get(&out_experiment[..]).unwrap();
                 //for which matches did that interesting event occur?
-                data_lookup.clone().into_iter()
-                    .filter(| ((_,idx),value) |  *value>0.0 && *idx==data_idx )
-                    .map(| ((time,_),_) | time ).collect::<Vec<_>>()
+                let to_insert = data.clone().into_iter()
+                    //filter first by idx matching the one in question
+                    .filter(| ((_,idx),_) |  *idx==data_idx )
+                    //and return only those rows
+                    .map(| ((time,_),_) | time ).collect::<Vec<_>>();
+                split_list.push(to_insert);
+                split_names.push(out_experiment.clone());
             },
         }; 
+    }
+
+    write!(stdout(),"{:>10}","Name").unwrap_or(());
+    write!(stdout(),"{:>10}","Output").unwrap_or(());
+    write!(stdout()," {:>5}","N").unwrap_or(());
+    write!(stdout()," {:>5}","E[x|a]").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=0)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=1)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=2)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=3)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=4)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=5)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=6)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=7)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=8)").unwrap_or(());
+    write!(stdout()," {:>5}","p(x=9)").unwrap_or(());
+    writeln!(stdout()).unwrap_or(());
+    for split_idx in 0..split_list.len(){
+        let relevant_matches = split_list[split_idx].clone();
+        let split_name = split_names[split_idx].clone();
         let relevant_len = relevant_matches.len();
         let out_len = out_idxs.len();
 
@@ -121,8 +177,8 @@ fn main() -> Result<(),String> {
                     match data_lookup.get(&(relevant_matches[i],col_idx))
                     {
                         None=>0.0,
-                        Some(x)=>*x,
-                    }
+                        Some(x)=> x.parse::<f32>().unwrap_or(0.0),
+                    },
                 );
                 out_m.set_column(out_counter,&column);
                 out_names.push( names[col_idx].clone());
@@ -135,19 +191,6 @@ fn main() -> Result<(),String> {
         let out_lambda = out_occurances/relevant_len as f32;
         let mut histogram: HashMap<i32,usize> = HashMap::new();
 
-        write!(stdout(),"{:>10}","Output").unwrap_or(());
-        write!(stdout()," {:>5}","E[x|a]").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=0)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=1)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=2)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=3)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=4)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=5)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=6)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=7)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=8)").unwrap_or(());
-        write!(stdout()," {:>5}","p(x=9)").unwrap_or(());
-        writeln!(stdout()).unwrap_or(());
 
         for idx in 0..out_names.len()
         {
@@ -158,7 +201,9 @@ fn main() -> Result<(),String> {
                 let count = histogram.entry(v.floor() as i32).or_insert(0);
                 *count = *count + 1;
             }
+            write!(stdout(),"{:>10}",split_name).unwrap_or(());
             write!(stdout(),"{:>10}",out_names[idx]).unwrap_or(());
+            write!(stdout(),"{:>5}",relevant_len).unwrap_or(());
             write!(stdout()," {:>5} ",
                 std::format!("{:2.2}",out_lambda[idx])).unwrap_or(());
             for x in 0..10{
@@ -171,7 +216,5 @@ fn main() -> Result<(),String> {
         }
     }
 
-
     Ok(())
-
 }
