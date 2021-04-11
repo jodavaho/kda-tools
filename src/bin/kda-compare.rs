a73f8f7d9c2e2453a122056683e4f04ace10a9aa
Josh Vander Hook <hook@jpl.nasa.gov>
2021-04-11 11:59:20 -0700

    cleaning up output and revamping for HPDI and HT

--- src/bin/kda-compare.rs
+++ src/bin/kda-compare.rs
@@ -1,10 +1,7 @@
 
 extern crate clap;
 use clap::{App,Arg};
-use nalgebra::DVector;
-use nalgebra::base::DMatrix;
 use std::collections::{HashMap,HashSet};
-use std::f32;
 use std::io::{Write,stdin,stdout,BufRead};
 use std::string::String;
 
@@ -17,18 +14,18 @@ fn main() -> Result<(),String> {
     let input_args = App::new("kda-compare")
         .version( &kda_tools::version()[..] )
         .author("Joshua Vander Hook <josh@vanderhook.info>")
-        .about("Conducts analysis on a KVC log. See https://github.com/jodavaho/kda-tools for more info")
+        .about("Some basic conditional probabilities and bayesian analysis on a KVC log. See https://github.com/jodavaho/kda-tools for more info. ")
         .arg(Arg::with_name("kda")
             .long("kda")
             .takes_value(false)
-            .help("Include the extra output KDA = (K+A)/D. Must include K,D, and A in the input data or this will fail")
+            .help("Include the extra output KDA = (K+A)/D. You'll need to have K, D, and A entries in your log or this will fail loudly.")
         )
         .arg(Arg::with_name("command")
             .short("c")
             .long("command")
             .value_name("COMMAND")
             .takes_value(true)
-            .help("Command a comparison like this: 'K (: [<+|-><item>] vs [<+|-><item>]+ )' e.g., 'K: +pistol vs +shotgun' to compare kills with shotguns vs pistols. use '_' to denote 'all items'. DEFAULT='K D A B' for all relevant stats")
+            .help("Command a comparison like this: 'K (: [<item>] vs [<item>] )' e.g., 'K: pistol vs shotgun' to compare kills with shotguns vs pistols. use '_' to denote 'baseline (avg over all matches)'.")
         )
         .get_matches();
     let local_sin = stdin();
@@ -51,168 +48,122 @@ fn main() -> Result<(),String> {
 
     let command = input_args.value_of("command").unwrap_or("K");
     eprintln!("processing: {}",command);
-    let mut inout :Vec<String> = command.split(":").map(|x| x.to_string()).collect();
-    if input_args.is_present("kda"){
-        inout.push("K".to_string());
-        inout.push("D".to_string());
-        inout.push("A".to_string());
-    } 
+    let inout :Vec<String> = command.split(":").map(|x| x.to_string()).collect();
  
     assert!(inout.len()<=2,"Got more than one ':', cannont process:{}",command);
     assert!(inout.len()>0,"Did not receive a valid command:{}",command);
 
-    let mut ins:Vec<String> = Vec::new();
-    if inout.len()==2{
-       ins= inout[1].split("vs").map(|x| x.trim().to_string()).collect() ;
-    } else {
-        assert_eq!(ins.len(),0);
+    let mut comparisons:Vec< Vec<String>> = Vec::new();
+    let all_comparisons:Vec<String>= inout[1].split("vs").map(|x| x.trim().to_string()).collect() ;
+    for grouping in all_comparisons{
+        //it's a string that's a list of items
+        comparisons.push( grouping.split_whitespace().map(|x| x.to_string()).collect());
     }
 
     //verify all input variables
-    let mut in_idxs:HashSet<usize> = HashSet::new();
-    for idx in 0..ins.len(){
-        let variable_name:String = ins[idx].clone();
-        eprintln!("Received input: {}",variable_name);
-        match &variable_name[..]{
-            //don't bother verifying reserved keywords at this stage
-            "_"|"*"=>(),
-            _=>{
-                assert!( idx_lookup.contains_key(&variable_name),std::format!("Variable '{}' not found in input data.",ins[idx]));
-                in_idxs.insert( *idx_lookup.get(&variable_name).unwrap() );
-            },
+    for grouping in comparisons.iter(){
+        //a grouping is a vec of strings
+        for item in grouping.iter(){
+            eprintln!("Received input: {}",item);
+            match &item[..]{
+                //don't bother verifying reserved keywords at this stage
+                "_"|"*"=>(),
+                _=>{
+                    assert!( idx_lookup.contains_key(item),std::format!("Variable '{}' not found in input data.",item) );
+                },
+            }
         }
     }
 
 
     //verify all output variables
-    let outs:Vec<String> = inout[0].split_whitespace().map(|x| x.to_string()).collect();
-    let mut out_idxs:HashSet<usize> = HashSet::new();
-    for idx in 0..outs.len(){
-        assert!( idx_lookup.contains_key(&outs[idx]),std::format!("Variable '{}' not found in input data.",outs[idx]));
-        out_idxs.insert( *idx_lookup.get(&outs[idx]).unwrap() );
+    let metrics:Vec<String> = inout[0].split_whitespace().map(|x| x.to_string()).collect();
+    for metric in metrics.iter(){
+        assert!( idx_lookup.contains_key(metric),std::format!("Requested output variable '{}' not found in input data.",metric));
     }
 
-    //create a handy data looker-upper
-    let data_lookup:HashMap<(usize,usize),String> = data.clone().into_iter().collect();
-    if ins.len()==0{
-        ins.push( "_".to_string() );
-    }
 
-    //create the relevant pairings to iterate over for experiments
-    let mut split_list:Vec<Vec<usize>> = Vec::new();
-    let mut split_names:Vec<String> = Vec::new();
-    for out_experiment in ins{
-        match &out_experiment[..]{
-            "*"=> {
-                eprintln!("Fetching: '*' as 'all individual inputs'");
-                //there's one special case, "*" which means all-to-all compare,
-                //in which case we can append all inputs as single experiments
-                for variable_name in names.clone(){
-                    let data_idx = *idx_lookup.get(&variable_name[..]).unwrap();
-                    //for which matches did that interesting event occur?
-                    let to_insert = data.clone().into_iter()
-                        //filter first by idx matching the one in question
-                        .filter(| ((_,idx),_) |  *idx==data_idx )
-                        //and return only those rows
-                        .map(| ((time,_),_) | time ).collect::<Vec<_>>();
-                    split_list.push(to_insert);
-                    split_names.push(variable_name.clone());
-                }
-            },
-            "_"=> {
-                eprintln!("Fetching: '_' as 'baseline'");
-                //compare vs all data "baseline"
-                split_list.push( (0..num_matches).collect::<Vec<_>>() );
-                split_names.push("Baseline".to_string());
-            },
-            _=>{
-                //user specifically requested some data by name.
-                //what column of the data corresponds to the  interesting one?
-                assert!(idx_lookup.contains_key(&out_experiment[..]),"Could not find {} in input data!",out_experiment);
-                eprintln!("Fetching {} by name",out_experiment);
-                let data_idx = *idx_lookup.get(&out_experiment[..]).unwrap();
-                //for which matches did that interesting event occur?
-                let to_insert = data.clone().into_iter()
-                    //filter first by idx matching the one in question
-                    .filter(| ((_,idx),_) |  *idx==data_idx )
-                    //and return only those rows
-                    .map(| ((time,_),_) | time ).collect::<Vec<_>>();
-                split_list.push(to_insert);
-                split_names.push(out_experiment.clone());
-            },
-        }; 
-    }
+    for metric in metrics.iter(){
+        //these are "rows"
+        //get the metrics / match
+        let metric_idx = idx_lookup.get(metric).unwrap();
+        for grouping in comparisons.iter(){
+            //initialize the metric "return value", which is a list of values we compare against
+            let mut grouping_name = metric.to_string()+":( ";
+            //calculate metrics for this grouping, starting with "all" and downselecting
+            let mut grouping_occurances : HashSet<_> = (0..num_matches).collect();
+            for item in grouping{
+                grouping_name+=&(item.to_string()+" ");
 
-    write!(stdout(),"{:>10}","Name").unwrap_or(());
-    write!(stdout(),"{:>10}","Output").unwrap_or(());
-    write!(stdout()," {:>5}","N").unwrap_or(());
-    write!(stdout()," {:>5}","E[x|a]").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=0)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=1)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=2)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=3)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=4)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=5)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=6)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=7)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=8)").unwrap_or(());
-    write!(stdout()," {:>5}","p(x=9)").unwrap_or(());
-    writeln!(stdout()).unwrap_or(());
-    for split_idx in 0..split_list.len(){
-        let relevant_matches = split_list[split_idx].clone();
-        let split_name = split_names[split_idx].clone();
-        let relevant_len = relevant_matches.len();
-        let out_len = out_idxs.len();
-
-        //now let's construct a matrix of all output variables across those matches
-        let mut out_m = DMatrix::<f32>::zeros( relevant_len,out_len);
-        let mut out_names = Vec::new();
-        let mut out_counter = 0;
-
-        for col_idx in 0..num_vars{
-            if out_idxs.contains(&col_idx){
-
-                let column = DVector::<f32>::from_fn(
-                    relevant_len, |i,_| 
-                    match data_lookup.get(&(relevant_matches[i],col_idx))
-                    {
-                        None=>0.0,
-                        Some(x)=> x.parse::<f32>().unwrap_or(0.0),
+                match &item[..]{
+                    "*"=> {
+                        eprintln!("Fetching: '*' as 'all individual inputs'");
+                        //there's one special case, "*" which means all-to-all compare,
+                        //which is weird and probably should be handled seperately ... it means n *rows* not n *columns* 
                     },
-                );
-                out_m.set_column(out_counter,&column);
-                out_names.push( names[col_idx].clone());
-                out_counter+=1;
-            } 
-        }
-        //calculate expected # occurances for poisson dist (k events / time period)
-        let out_occurances = out_m.row_sum();
-
-        let out_lambda = out_occurances/relevant_len as f32;
-        let mut histogram: HashMap<i32,usize> = HashMap::new();
-
-
-        for idx in 0..out_names.len()
-        {
-            //baseline / condition 1
-            histogram.clear();
-            for m in 0..relevant_len{
-                let v = out_m.get((m,idx)).unwrap();
-                let count = histogram.entry(v.floor() as i32).or_insert(0);
-                *count = *count + 1;
+                    "_"=> {
+                        eprintln!("Fetching: '_' as 'baseline'");
+                        //compare vs all data "baseline"
+                        //relevent_matches.push( (0..num_matches).collect::<Vec<_>>() );
+                        //split_names.push("Baseline".to_string());
+                    },
+                    _=>{
+                        //user specifically requested some data by name as part of a multi-item grouping
+                        //what column of the data corresponds to the  interesting one?
+                        assert!(idx_lookup.contains_key(item),"Could not find {} in input data!",item);
+                        eprintln!("Fetching {} by name",item);
+                        let data_idx = *idx_lookup.get(item).unwrap();
+                        //for which matches did that item appear?
+                        let item_occurances = data.iter()
+                            //filter first by idx matching the one in question
+                            .filter(| ((_,idx),_) |  *idx==data_idx )
+                            //and return only those rows
+                            .map(| ((time,_),_) | *time ).collect::<HashSet<usize>>();
+                        //use intersetino for AND relationship
+                        grouping_occurances.retain(|x| item_occurances.contains(x));
+                    },
+                }; 
             }
-            write!(stdout(),"{:>10}",split_name).unwrap_or(());
-            write!(stdout(),"{:>10}",out_names[idx]).unwrap_or(());
-            write!(stdout(),"{:>5}",relevant_len).unwrap_or(());
-            write!(stdout()," {:>5} ",
-                std::format!("{:2.2}",out_lambda[idx])).unwrap_or(());
-            for x in 0..10{
-                let count_x = *histogram.entry(x).or_insert(0);
-                write!(stdout()," {:>5} ",
-                    std::format!("{:2.2}",count_x as f32 / relevant_len as f32)).unwrap_or(());
-            }
-            //comparison / condition 2
-            writeln!(stdout(),"").unwrap_or(());
+            grouping_name+=")";
+            let all_matches = (0..num_matches).collect::<HashSet<_>>();
+            let grouping_non_occurances = all_matches.symmetric_difference(&grouping_occurances).collect::<HashSet<_>>();
+
+            //now we have a grouping for which to request data later. 
+            //what about zeros ... times when metric did not occur but grouping did? The rest of those are just diff in len
+            let metric_values = data.iter()
+                                    //filter first by idx matching the one in question
+                                    .filter(| ((match_number,variable_idx),_) |  grouping_occurances.contains(match_number) && variable_idx == metric_idx)
+                                    //and return only those rows and values
+                                    .map(| ((_,_),v) | v ).collect::<Vec<_>>();
+            let metric_non_values = data.iter()
+                                    //filter first by idx matching the one in question
+                                    .filter(| ((match_number,variable_idx),_) |  grouping_non_occurances.contains(match_number) && variable_idx == metric_idx )
+                                    //and return only those rows and values
+                                    .map(| ((_,_),v) | v ).collect::<Vec<_>>();
+            //now we have everything to do a with/without comparison for this grouping
+            let n_with = grouping_occurances.len();
+            let n_without = grouping_non_occurances.len();
+            assert!(n_with + n_without == num_matches);
+            eprintln!("N with grouping: {}",n_with);
+            eprintln!("N w/o grouping: {}",n_without);
+
+            let metric_occured_with_grouping = metric_values.len();
+            let metric_occured_without_grouping = metric_non_values.len();
+            eprintln!("matches with at least a metric value & grouping: {}",metric_occured_with_grouping);
+            eprintln!("matches with at least a metric value & w/o group: {}",metric_occured_without_grouping);
+            assert!(n_with >= metric_values.len(),"Bug: Got more metric entries than grouping entries");
+            assert!(n_without >= metric_non_values.len(),"Bug: Got more metric entries than grouping entries");
+
+            //we may have missed some matches for which the metric never occured (no kills is common in matches)
+            //for those, assume zeros, so we just need to know *how many* zeros to pad
+            let n_extra_zeros_with = n_with - metric_values.len();
+            let n_extra_zeros_without = n_without - metric_non_values.len();
+            eprintln!("Assumed zeros for matches w/grouping: {}",n_extra_zeros_with);
+            eprintln!("Assumed zeros for matches w/o grouping: {}",n_extra_zeros_without);
+
+            let mut output_string = String::new();
+            writeln!(stdout(), "{}", output_string).unwrap_or(());
+
         }
     }
 
